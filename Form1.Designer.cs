1d8db6720eaa6612e3a6d7bff386fa3ebac6d830
JhonMeza7 <jhon.meza@epn.edu.ec>
2022-03-01 11:17:35 -0500

    agragar txtboxs contador de papas textlist boton confirmar

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,210 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.lblContador1 = new System.Windows.Forms.Label();
+            this.lblContador2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtCi = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Apellido:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "CI:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(246, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Hamburguesas:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(246, 95);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Salchipapa:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 282);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "CONFIRMAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(447, 41);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(38, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "+";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(376, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(38, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(376, 88);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(38, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "-";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(447, 88);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(38, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "+";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // lblContador1
+            // 
+            this.lblContador1.AutoSize = true;
+            this.lblContador1.Location = new System.Drawing.Point(427, 44);
+            this.lblContador1.Name = "lblContador1";
+            this.lblContador1.Size = new System.Drawing.Size(14, 16);
+            this.lblContador1.TabIndex = 10;
+            this.lblContador1.Text = "0";
+            // 
+            // lblContador2
+            // 
+            this.lblContador2.AutoSize = true;
+            this.lblContador2.Location = new System.Drawing.Point(427, 95);
+            this.lblContador2.Name = "lblContador2";
+            this.lblContador2.Size = new System.Drawing.Size(14, 16);
+            this.lblContador2.TabIndex = 11;
+            this.lblContador2.Text = "0";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(118, 41);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 22);
+            this.txtNombre.TabIndex = 12;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(119, 88);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 22);
+            this.txtApellido.TabIndex = 13;
+            // 
+            // txtCi
+            // 
+            this.txtCi.Location = new System.Drawing.Point(119, 131);
+            this.txtCi.Name = "txtCi";
+            this.txtCi.Size = new System.Drawing.Size(100, 22);
+            this.txtCi.TabIndex = 14;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(229, 282);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(417, 132);
+            this.listBox1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtCi);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblContador2);
+            this.Controls.Add(this.lblContador1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label lblContador1;
+        private System.Windows.Forms.Label lblContador2;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtCi;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,105 @@ using System.Windows.Forms;
 
 namespace Poliburguer_feed_yourself
 {
+    public struct Persona
+    {
+        public string Nombre;
+        public string Apellido;
+        public string CI;
+    }
+    public struct Categoria
+    {
+        public string nombre;
+    }
+    public struct Producto
+    {
+        public string nombre;
+        public int stock;
+        public float precio;
+        public Categoria Categoria;
+        public int id;
+    }
+    public struct Fecha
+    {
+        public string dia;
+        public string mes;
+        public string anio;
+    }
+    public struct Factura
+    {
+        public string nombre;
+        public string CI;
+        public string fecha;
+        public double subtotal;
+        public double total;
+        public string pedidos;
+    }
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+
+                if (string.IsNullOrEmpty(txtNombre.Text) || string.IsNullOrEmpty(txtCi.Text))
+                {
+                    MessageBox.Show("Algunos parametros en blanco");
+                }
+                else
+                {
+                    Persona persona;
+                    persona.Nombre = txtNombre.Text;
+                    persona.Apellido = txtApellido.Text;
+                    persona.CI = txtCi.Text;
+                    subtotal += contadorHamgurguesa * productos[0].precio;
+                    stockHamburguesa -= contadorHamgurguesa;
+                    subtotal += contadorPapa * productos[1].precio;
+                    stockPapa -= contadorPapa;
+             
+
+                    total = subtotal + subtotal * 0.12;
+                    DateTime now = DateTime.UtcNow.Date;
+                    Fecha fechaActual;
+                    fechaActual.dia = now.ToString("dd");
+                    fechaActual.mes = now.ToString("MM");
+                    fechaActual.anio = now.ToString("yyyy");
+
+                    listBox1.Items.Add(factura.nombre);
+                    listBox1.Items.Add(factura.CI);
+                    listBox1.Items.Add(factura.fecha);
+
+
+                    
+
+                    listBox1.Items.Add("Subtotal: " + factura.subtotal);
+                    listBox1.Items.Add("Total: " + factura.total);
+
+                }
+                txtNombre.Text = "";
+                txtApellido.Text = "";
+                txtCi.Text = "";
+                lblContador1.Text = "0";
+                lblContador2.Text = "0";
+                
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ocurrio algun error");
+                throw;
+            }
         }
     }
 }
