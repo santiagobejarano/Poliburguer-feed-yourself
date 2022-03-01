3c7c46b7819d2552895b031858e5201cbd6f5016
1-abdu <tarek.abdelfatah@epn.edu.ec>
2022-03-01 11:26:38 -0500

    agregar las estructuras

--- Form1.cs
+++ Form1.cs
@@ -60,6 +60,59 @@ namespace Poliburguer_feed_yourself
         {
             try
             {
+                Categoria cat1, cat2, cat3;
+                cat1.nombre = "Dulces";
+                cat2.nombre = "Bebida";
+                cat3.nombre = "Comida";
+
+                Producto[] productos = new Producto[8];
+                productos[0].nombre = "Hamburguesa";
+                productos[0].stock = stockHamburguesa;
+                productos[0].precio = 6;
+                productos[0].Categoria = cat3;
+                productos[0].id = 10001;
+
+                productos[1].nombre = "Salchipapa";
+                productos[1].stock = stockPapa;
+                productos[1].precio = 3;
+                productos[1].Categoria = cat3;
+                productos[1].id = 10002;
+
+                productos[2].nombre = "Hotdog";
+                productos[2].stock = stockHotDog;
+                productos[2].precio = 5;
+                productos[2].Categoria = cat3;
+                productos[2].id = 10003;
+
+                productos[3].nombre = "Cola";
+                productos[3].stock = stockCola;
+                productos[3].precio = 2;
+                productos[3].Categoria = cat2;
+                productos[3].id = 10004;
+
+                productos[4].nombre = "Agua";
+                productos[4].stock = stockAgua;
+                productos[4].precio = 1;
+                productos[4].Categoria = cat2;
+                productos[4].id = 10005;
+
+                productos[5].nombre = "Te";
+                productos[5].stock = stockTe;
+                productos[5].precio = 2;
+                productos[5].Categoria = cat2;
+                productos[5].id = 10006;
+
+                productos[6].nombre = "Chocolate";
+                productos[6].stock = stockChocolate;
+                productos[6].precio = 3;
+                productos[6].Categoria = cat1;
+                productos[6].id = 10007;
+
+                productos[7].nombre = "Helado";
+                productos[7].stock = stockHelado;
+                productos[7].precio = 4;
+                productos[7].Categoria = cat1;
+                productos[7].id = 10008;
 
 
 
