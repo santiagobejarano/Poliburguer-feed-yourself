8a333f10c7b23a98a5436c3f129da9a6a94d6631
1-abdu <tarek.abdelfatah@epn.edu.ec>
2022-03-01 12:06:10 -0500

    Terminar del parte del diseño del GUI Final

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -822,7 +822,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1120, 585);
+            this.ClientSize = new System.Drawing.Size(1120, 560);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.lblFactura);
             this.Controls.Add(this.panel5);
