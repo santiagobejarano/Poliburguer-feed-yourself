ad798d0727e79c04f97cbb39faf9d7f1c692aac6
1-abdu <tarek.abdelfatah@epn.edu.ec>
2022-03-01 15:10:52 -0500

    FYS Final

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -829,6 +829,7 @@
             // 
             // listBox1
             // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(887, 191);
             this.listBox1.Name = "listBox1";
